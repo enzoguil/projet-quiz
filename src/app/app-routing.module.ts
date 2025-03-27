5bac941f935369571c19ec49990facd415b77a5e
Vincent-To <vincent.tournelier@epitech.eu>
2025-03-27 09:09:22 +0100

    opt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { QuizComponent } from './components/quiz/quiz.component';
+import { QuizConfigComponent } from './components/quiz-config/quiz-config.component';
 
 const routes: Routes = [
   {path : 'quiz/:amount/:category/:type:/:difficulty', component: QuizComponent},
+  {path : '', component: QuizConfigComponent},
 ];
 
 @NgModule({
