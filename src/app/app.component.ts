e81268f43d5ebba1a3d162e586c15327e89ec167
Vincent-To <vincent.tournelier@epitech.eu>
2025-03-27 08:52:31 +0100

    opt change quiz directory location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuizService } from './services/quiz.service';
+import { DarkModeService } from './services/dark-mode.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+/*export class AppComponent {
   title = 'projet-quiz';
+}*/
+export class AppComponent implements OnInit {
+  quizConfig: any;
+
+  constructor(private quizService: QuizService, public darkModeService: DarkModeService) {}
+
+  toggleDarkMode() {
+    this.darkModeService.toggleDarkMode();
+  }
+
+  get isDarkMode() {
+    return this.darkModeService.isDarkModeEnabled();
+  }
+
+  ngOnInit() {
+    this.quizService.quizConfig$.subscribe(config => {
+      this.quizConfig = config;
+      console.log('Quiz config received:', config);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuizComponent } from './composants/quiz/quiz.component';
-import { HttpClientModule } from '@angular/common/http';
+import { QuizConfigComponent } from './components/quiz-config/quiz-config.component';
+import { QuizPageComponent } from './components/quiz-page/quiz-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuizComponent
+    QuizConfigComponent,
+    QuizPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
